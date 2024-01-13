8fa2c475e73290e1b47626a3521e123d386efea0
Eiji Hachiya <hachiyae@gmail.com>
2024-01-13 10:24:46 +0900

    feat: add CssBaseline

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import {CssBaseline} from "@mui/material";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <CssBaseline/>
         <Main />
         <NextScript />
       </body>
