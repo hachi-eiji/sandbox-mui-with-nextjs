2f94aa26d578011e272a6ed2217066e83b9435cc
Eiji Hachiya <hachiyae@gmail.com>
2024-01-13 13:23:59 +0900

    refactor: move

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,8 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import {AppThemeProvider} from "@/pages/themes/AppThemeProvider";
-import UserThemeProvider from "@/pages/themes/UserThemeProvider";
+import {AppThemeProvider} from "@/themes/AppThemeProvider";
+import UserThemeProvider from "@/themes/UserThemeProvider";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
