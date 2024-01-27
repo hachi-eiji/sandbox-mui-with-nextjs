b3f3242883763f9e21f002729230c3e58511e422
Eiji Hachiya <hachiyae@gmail.com>
2024-01-27 21:33:10 +0900

    feat: まず基本的なところ

---
+++ src/pages/autocomplete/index.tsx
@@ -0,0 +1,38 @@
+import { Autocomplete, Box, TextField } from "@mui/material";
+import * as React from "react";
+import { useState } from "react";
+
+export default function AutoCompletePage() {
+  const values: { id: number; label: string }[] = [];
+  for (let i = 0; i < 20; i++) {
+    values.push({ id: i, label: `option label ${i}` });
+  }
+
+  const [selectLabel, setSelectLabel] = useState<string>();
+
+  return (
+    <Box sx={{ mt: 2, ml: 2, width: "300px" }}>
+      <Box>
+        selected label is <span style={{ fontWeight: "bold" }}>{selectLabel ?? "none"}</span>
+      </Box>
+      <Autocomplete
+        size="small"
+        options={values}
+        isOptionEqualToValue={(option, value) => option.id === value.id}
+        renderInput={(params) => <TextField {...params} placeholder="select opion" />}
+        onChange={(event, value, reason) => {
+          switch (reason) {
+            case "selectOption":
+              setSelectLabel(value?.label);
+              break;
+            case "clear":
+              setSelectLabel(undefined);
+              break;
+            default:
+            //none
+          }
+        }}
+      />
+    </Box>
+  );
+}
