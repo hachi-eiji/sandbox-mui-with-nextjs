b348c1d30c7418ae02dd6b4a77bfbee588238707
Eiji Hachiya <hachiyae@gmail.com>
2024-01-31 00:21:10 +0900

    feat: カレンダー

--- src/pages/datefield/index.tsx
+++ src/pages/datefield/index.tsx
@@ -2,13 +2,22 @@ import { useState } from "react";
 import { Box } from "@mui/material";
 import AppDatePicker, { dateErrorMessage } from "@/components/AppDatePicker";
 import { format } from "date-fns/format";
+import { css } from "@emotion/react";
+
+const style = {
+  container: css({
+    display: "flex",
+    flexDirection: "column",
+    width: "350px",
+  }),
+};
 
 export default function DateFieldPage() {
   const [error, setError] = useState<string>();
   const [selectDate, setSelectDate] = useState<Date | undefined | null>(new Date());
 
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", width: "350px" }}>
+    <Box css={[style.container]}>
       <Box>{selectDate && format(selectDate, "yyyy年MM月dd日")}</Box>
       <Box>{error}</Box>
       <AppDatePicker
